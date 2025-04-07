8dafe99c05faa41686472786eb78151a05ba8409
孤岛 <zouzhangpeng@163.com>
2025-04-07 21:42:06 +0800

    角色移动

--- scenes/player/Player.cs
+++ scenes/player/Player.cs
@@ -1,6 +1,20 @@
 using Godot;
-using System;
 
 public partial class Player : CharacterBody2D
 {
+    private readonly float MaxSpeed = 200f;
+    
+    public override void _Process(double delta)
+    {
+        Vector2 direction = GetMovementVector().Normalized();
+        Velocity = direction * MaxSpeed;
+        MoveAndSlide();
+    }
+
+    private Vector2 GetMovementVector()
+    {
+        float xMovement = Input.GetActionStrength("MoveRight") - Input.GetActionStrength("MoveLeft");
+        float yMovemment = Input.GetActionStrength("MoveDown") - Input.GetActionStrength("MoveUp");
+        return new Vector2(xMovement, yMovemment);
+    }
 }
