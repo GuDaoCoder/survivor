eda62e882c90ee18e5cbfd58610ce6fade08e493
zouzhangpeng <zouzhangpeng@163.com>
2025-04-12 17:15:13 +0800

    随机生成敌人

---
+++ scenes/manager/EnemyManager.cs
@@ -0,0 +1,39 @@
+using System;
+using Godot;
+
+namespace survivor.scenes.manager;
+
+public partial class EnemyManager : Node
+{
+
+    [Export]
+    public float MaxRange = 350f;
+
+    [Export]
+    public Timer Timer;
+
+    [Export]
+    public PackedScene BasicEnemyPackedScene;
+    
+    public override void _Ready()
+    {
+        Timer.Timeout += OnTimerTimeout;
+    }
+
+    private void OnTimerTimeout()
+    {
+        Player player = GetTree().GetFirstNodeInGroup("player") as Player;
+        if (player == null)
+        {
+            return;
+        }
+
+        Vector2 randomDirection = Vector2.Right.Rotated((float)GD.RandRange(0,Double.Tau));
+        Vector2 spawnPosition= player.GlobalPosition - randomDirection * MaxRange;
+
+        BasicEnemy basicEnemy = BasicEnemyPackedScene.Instantiate<BasicEnemy>();
+        GetParent().AddChild(basicEnemy);
+        basicEnemy.GlobalPosition = spawnPosition;
+
+    }
+}
