0561e3b857183aee3e01d60a7881e99495178bf0
孤岛 <zouzhangpeng@163.com>
2025-04-16 22:27:13 +0800

    实现敌人死亡掉落经验

---
+++ scenes/component/VailDropComponent.cs
@@ -0,0 +1,32 @@
+using Godot;
+using survivor.scenes.component;
+using survivor.scenes.game_object.experience_vial;
+
+namespace survivor;
+
+public partial class VailDropComponent : Node
+{
+    [Export] public HealthComponent HealthComponent;
+
+    [Export] public PackedScene ExperienceVailScene;
+
+    public override void _Ready()
+    {
+        HealthComponent.Die += OnDie;
+    }
+
+    public void OnDie()
+    {
+        if (ExperienceVailScene == null)
+        {
+            return;
+        }
+
+        if (Owner is Node2D node)
+        {
+            ExperienceVial experienceVial = ExperienceVailScene.Instantiate<ExperienceVial>();
+            experienceVial.GlobalPosition = node.GlobalPosition;
+            Owner.GetParent().AddChild(experienceVial);
+        }
+    }
+}
